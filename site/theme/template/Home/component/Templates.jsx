d802d315d7d1dbe4e180df9c29d99ea72735ced9
jljsj <liangjun.jiang@alipay.com>
2018-01-30 19:38:17 +0800

    update

--- site/theme/template/Home/component/Templates.jsx
+++ site/theme/template/Home/component/Templates.jsx
@@ -4,8 +4,8 @@ import { Row, Col } from 'antd';
 import GitHub from 'github-api';
 
 export default class Templates extends React.PureComponent {
-  constructor(){
-    
+  constructor(props){
+    super(props);
   }
   render(){
     return (

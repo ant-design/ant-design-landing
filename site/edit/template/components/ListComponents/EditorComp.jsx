8f7c472368414e8ccc18cd043e0c0926f6276e7c
jljsj <liangjun.jiang@alipay.com>
2019-11-15 21:57:15 +0800

    rm console

--- site/edit/template/components/ListComponents/EditorComp.jsx
+++ site/edit/template/components/ListComponents/EditorComp.jsx
@@ -43,7 +43,6 @@ class EditorComp extends React.Component {
     };
     newTemplateData.data.style = (newTemplateData.data.style || []).filter(c => c.id !== cb.id);
     newTemplateData.data.style.push(data);
-    console.log(Date.now());
     dispatch(actions.setTemplateData(newTemplateData));
   }
 
@@ -84,7 +83,6 @@ class EditorComp extends React.Component {
   }
 
   render() {
-    console.log(Date.now());
     const { currentEditData, mediaStateSelect, location } = this.props;
     const isCN = isZhCN(location.pathname);
     if (!currentEditData) {
